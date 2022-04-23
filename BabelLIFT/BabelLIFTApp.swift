c0ae42ee7a715f1adb31cd6193a873b3e5581cce
Nicholas Ngai <nicholas@ngai.me>
2022-04-23 11:31:24 -0700

    Add Swift + SwiftUI initial setup

---
+++ BabelLIFT/BabelLIFTApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabelLIFTApp.swift
+//  BabelLIFT
+//
+//  Created by Nicholas Ngai on 4/23/22.
+//
+
+import SwiftUI
+
+@main
+struct BabelLIFTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BabelLIFT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BabelLIFT
+//
+//  Created by Nicholas Ngai on 4/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
