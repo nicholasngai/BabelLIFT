6bd7d6efaa732aaf05bd25a2faacb9497daa1a3d
Nicholas Ngai <nicholas@ngai.me>
2022-04-24 19:22:09 -0700

    Add LIFTParser.swift and use in fileImporter

--- BabelLIFT/ContentView.swift
+++ BabelLIFT/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isShowingFileImporter = false
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Button(action: { isShowingFileImporter.toggle(); print(isShowingFileImporter) }) {
+                    Text("Load new lexicon...")
+                }
+            }
+            .navigationTitle("BabelLIFT")
+        }
+        // TODO Restrict to .lift.
+        .fileImporter(isPresented: $isShowingFileImporter, allowedContentTypes: [.item], onCompletion:handleLoadLift)
+
+    }
+
+    private func handleLoadLift(result: Result<URL, Error>) {
+        do {
+            let url = try result.get()
+            let data = try Data(contentsOf: url)
+            let liftParser = LIFTParser(data: data)
+            liftParser.parse()
+            print(liftParser.getParsed())
+        } catch {
+            print(error)
+        }
     }
 }
 
---
+++ BabelLIFT/LIFTParser.swift
@@ -0,0 +1,79 @@
+//
+//  LIFTParser.swift
+//  BabelLIFT
+//
+//  Created by Nicholas Ngai on 4/24/22.
+//
+
+import Foundation
+
+struct LIFTEntry {
+    let lexicalUnits: [String: String]
+    let senses: [String: String]
+}
+
+struct LIFT {
+    let entries: [LIFTEntry]
+}
+
+class LIFTParser: XMLParser {
+    private var parsed: LIFT? = nil
+
+    private var path: [String] = []
+    private var curLang: String? = nil
+    private var curLexicalUnits: [String: String]? = nil
+    private var curSenses: [String: String]? = nil
+    private var curEntries: [LIFTEntry] = []
+
+    override init(data: Data) {
+        super.init(data: data)
+        self.delegate = self
+    }
+}
+
+extension LIFTParser: XMLParserDelegate {
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String: String] = [:]) {
+        path.append(elementName)
+
+        switch path {
+        case ["lift", "entry"]:
+            curLexicalUnits = [:]
+            curSenses = [:]
+        case ["lift", "entry", "lexical-unit", "form"], ["lift", "entry", "sense", "gloss"]:
+            curLang = attributeDict["lang"]
+        default:
+            print("Warning: Ignored LIFT path: \(path)")
+        }
+    }
+
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        switch path {
+        case ["lift"]:
+            parsed = LIFT(entries: curEntries)
+        case ["lift", "entry"]:
+            let entry = LIFTEntry(lexicalUnits: curLexicalUnits!, senses: curSenses!)
+            curEntries.append(entry)
+        case ["lift", "entry", "lexical-unit", "form"], ["lift", "entry", "sense", "gloss"]:
+            curLang = nil
+        default:
+            print("Warning: Ignored LIFT path: \(path)")
+        }
+
+        path.removeLast()
+    }
+
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        switch path {
+        case ["lift", "entry", "lexical-unit", "form", "text"]:
+            curLexicalUnits![curLang!] = string
+        case ["lift", "entry", "sense", "gloss", "text"]:
+            curSenses![curLang!] = string
+        default:
+            print("Warning: Ignored LIFT path: \(path)")
+        }
+    }
+
+    func getParsed() -> LIFT {
+        return parsed!
+    }
+}
