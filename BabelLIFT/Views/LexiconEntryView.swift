5e418a34d078ff134a628be840f83741934543a6
Nicholas Ngai <nicholas@ngai.me>
2022-04-26 14:38:49 -0700

    Handle multiple senses per lexical unit

--- BabelLIFT/Views/LexiconEntryView.swift
+++ BabelLIFT/Views/LexiconEntryView.swift
@@ -8,24 +8,30 @@ struct LexiconEntryView: View {
         List {
             if senses.count > 1 {
                 ForEach(0 ..< senses.count, id: \.self) { i in
+                    let sense = senses[i]
                     Section(String(i + 1)) {
+                        renderSense(sense)
                     }
                 }
             } else if senses.count == 1 {
                 let sense = senses[0]
-                ForEach(sense.keys.sorted(), id: \.self) { lang in
-                    HStack {
-                        Text(lang)
-                            .opacity(0.5)
-                        Text(sense[lang]!)
-                            .frame(maxWidth: .infinity, alignment: .trailing)
-                    }
-                }
+                renderSense(sense)
             }
         }
         .listStyle(.plain)
         .navigationTitle(lexicalUnit)
         .navigationBarTitleDisplayMode(.inline)
+    }
+
+    private func renderSense(_ sense: [String: String]) -> some View {
+        return ForEach(sense.keys.sorted(), id: \.self) { lang in
+            HStack {
+                Text(lang)
+                    .opacity(0.5)
+                Text(sense[lang]!)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+            }
+        }
     }
 }
 
